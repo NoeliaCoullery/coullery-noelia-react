78e6a6fa119f9cd3238ff03864bed51ffb0018c3
Noelia Coullery <noe.coullery@gmail.com>
2024-07-09 15:56:31 -0300

    cambios para vercel

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+
+    return(
+        <>
+        <ul>
+            <li>Inicio</li>
+            <li>Contacto</li>
+            <li>Productos</li>
+            <li>Mi Cuenta</li>
+
+        </ul>
+        </>
+    );
+}
+
+export default Navbar; 
